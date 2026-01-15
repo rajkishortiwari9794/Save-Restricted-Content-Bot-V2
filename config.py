3307a4d8909a7dec86541978d9bcfcec49b35eec
rajkishortiwari9794 <rajkishortiwari9794@gmail.com>
2026-01-15 14:25:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7889923522:AAFqnudJuXuuQId8kBej3NUnHJNWMAk62SU"
 OWNER_ID = list(map(int, getenv("", "5716292248 7604081669 7080075962").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jeetnarayantiwari5:FGXfMWTnAsaEB4u2@cluster0.2whtu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1003075641319")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002401910672"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003075641319"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "10000"))
