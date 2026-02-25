419e7f59cc3f961b35014cab7e717092ca314aa5
rajkishortiwari9794 <rajkishortiwari9794@gmail.com>
2026-02-25 14:36:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask
+from pyrogram import filters
+from pyrogram.errors import UserNotParticipant
+from config import CHANNEL_ID
 
 app = Flask(__name__)
 
@@ -33,3 +36,12 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+@app.on_message(filters.private & ~filters.command("start"), group=-1)
+async def global_force_sub(client, message):
+    try:
+        await client.get_chat_member(CHANNEL_ID, message.from_user.id)
+    except UserNotParticipant:
+        await message.reply_text(
+            "⚠️ Pehle hamara channel join karo, phir bot use karo."
+        )
+        return
